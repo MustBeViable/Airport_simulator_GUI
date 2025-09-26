10f4218713e318826386e329a446bf996a3b5457
Elias <elias1rinne@gmail.com>
2025-09-26 12:38:50 +0300

    add animation abstract methods

--- src/main/java/view/IVisualisation.java
+++ src/main/java/view/IVisualisation.java
@@ -1,8 +1,14 @@
 package view;
 
+import simu.model.EventType;
+
 public interface IVisualisation {
 	public void clearDisplay();
 	public void newCustomer();
     public void newCustomerCheckin();
+    public void newCustomerLuggageDrop(double startX, double startY, boolean isPriority);
+    public void customerAnimationToSecurity(boolean isPriority, EventType from);
+    public void customerAnimationToPassport(boolean isPriority, EventType from);
+    public void customerAnimationToGate(EventType from);
 }
 
