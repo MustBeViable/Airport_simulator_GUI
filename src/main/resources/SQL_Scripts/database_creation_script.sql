a1779d2f00566e419ac3f2bd949f2b0fe296c18b
Elias <elias1rinne@gmail.com>
2025-09-26 16:44:28 +0300

    add database creation script

---
+++ src/main/resources/SQL_Scripts/database_creation_script.sql
@@ -0,0 +1,62 @@
+DROP DATABASE IF EXISTS airport_simulator;
+CREATE DATABASE airport_simulator;
+USE airport_simulator;
+
+
+DROP TABLE IF EXISTS run_statistics;
+DROP TABLE IF EXISTS run;
+
+CREATE TABLE run (
+    id INT NOT NULL AUTO_INCREMENT,
+    check_in_queues_count INT NOT NULL,
+    luggage_drop_count INT NOT NULL,
+    priority_luggage_drop_count INT NOT NULL,
+    security_count INT NOT NULL,
+    priority_security_count INT NOT NULL,
+    passport_control_count INT NOT NULL,
+    priority_passport_control_count INT NOT NULL,
+    gate_count INT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE run_statistics (
+    id INT NOT NULL AUTO_INCREMENT,
+    run_id INT NOT NULL,
+    check_in_queue_max_length INT NOT NULL,
+    check_in_queue_min_length INT NOT NULL,
+    check_in_queue_average_length DECIMAL(10,3) NOT NULL,
+
+    luggage_drop_queue_max_length INT NOT NULL,
+    luggage_drop_queue_min_length INT NOT NULL,
+    luggage_drop_queue_average_length DECIMAL(10,3) NOT NULL,
+
+    priority_luggage_drop_queue_max_length INT NOT NULL,
+    priority_luggage_drop_queue_min_length INT NOT NULL,
+    priority_luggage_drop_queue_average_length DECIMAL(10,3) NOT NULL,
+
+    security_queue_max_length INT NOT NULL,
+    security_queue_min_length INT NOT NULL,
+    security_queue_average_length DECIMAL(10,3) NOT NULL,
+
+    priority_security_queue_max_length INT NOT NULL,
+    priority_security_queue_min_length INT NOT NULL,
+    priority_security_queue_average_length DECIMAL(10,3) NOT NULL,
+
+    passport_control_queue_max_length INT NOT NULL,
+    passport_control_queue_min_length INT NOT NULL,
+    passport_control_queue_average_length DECIMAL(10,3) NOT NULL,
+
+    priority_passport_control_queue_max_length INT NOT NULL,
+    priority_passport_control_queue_min_length INT NOT NULL,
+    priority_passport_control_queue_average_length DECIMAL(10,3) NOT NULL,
+
+    gate_queue_max_length INT NOT NULL,
+    gate_queue_min_length INT NOT NULL,
+    gate_queue_average_length DECIMAL(10,3) NOT NULL,
+
+    PRIMARY KEY (id),
+    CONSTRAINT fk_run_stats_run
+        FOREIGN KEY (run_id) REFERENCES run(id)
+            ON DELETE CASCADE ON UPDATE CASCADE,
+    UNIQUE (run_id)
+);
