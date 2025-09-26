d308b8a220930efb6ba2d4c421912011a8e3524d
Elias <elias1rinne@gmail.com>
2025-09-26 12:43:07 +0300

    Add background layer to keep image in place and preventing redundant updating for it

--- src/main/java/view/VisualisationBG.java
+++ src/main/java/view/VisualisationBG.java
@@ -5,11 +5,12 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
+import simu.model.EventType;
 
 public class VisualisationBG extends Canvas implements IVisualisation {
     private GraphicsContext gc;
     int customerCount = 0;
-    private Image canvasBackGround = new Image("/images/canvas_pohja.png");
+    private Image canvasBackGround = new Image("/images/canvas_pohja2.png");
 
     public VisualisationBG(int w, int h) {
         super(w, h);
@@ -24,11 +25,32 @@ public class VisualisationBG extends Canvas implements IVisualisation {
 
     @Override
     public void newCustomer() {
-        customerCount++;
+
     }
 
     @Override
     public void newCustomerCheckin() {
+
+    }
+
+    @Override
+    public void newCustomerLuggageDrop(double startX, double startY, boolean isPriority) {
+
+    }
+
+    @Override
+    public void customerAnimationToSecurity(boolean isPriority, EventType from) {
+
+    }
+
+
+    @Override
+    public void customerAnimationToPassport(boolean isPriority, EventType from) {
+
+    }
+
+    @Override
+    public void customerAnimationToGate(EventType from) {
 
     }
 
