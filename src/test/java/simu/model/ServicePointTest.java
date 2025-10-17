4934b176a31ccb3befc4d8981497ed3fcc794f71
sakari Honkavaara <Sakari.honkavaara@metropolia.fi>
2025-10-17 15:17:12 +0300

    More tests for servicepoint, and added javadoc for servicepoint and servicepointtest

--- src/test/java/simu/model/ServicePointTest.java
+++ src/test/java/simu/model/ServicePointTest.java
@@ -13,6 +13,12 @@ import simu.framework.Trace;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Unit tests for the ServicePoint class.
+ *
+ * These tests verify queue assignment, service start and event scheduling,
+ * queue removal, and statistics tracking.
+ */
 class ServicePointTest {
 
     private ContinuousGenerator generator;
@@ -40,7 +46,9 @@ class ServicePointTest {
         System.out.println("**--- Test finished ---**");
     }
 
-
+    /**
+     * Verifies that passengers are distributed into the shortest queues.
+     */
     @Test
     void passengerGoesIntoShortestQueue() {
         EventList eventList = new EventList() {
@@ -65,9 +73,12 @@ class ServicePointTest {
         assertFalse(sp.isQueueEmpty(1), "Queue 1 is empty");
         assertFalse(sp.isQueueEmpty(2), "Queue 2 is empty");
         assertFalse(sp.isQueueEmpty(3), "Queue 3 is empty");
-
     }
 
+    /**
+     * Verifies that starting service reserves the queue
+     * and schedules a completion event.
+     */
     @Test
     void beginsServiceAndEventIsScheduledTest() {
         // Collecting events to list
@@ -90,5 +101,62 @@ class ServicePointTest {
 
         assertTrue(sp.isReserved(0), "Queue 0 should be reserved");
         assertEquals(1, capturedEvents.size(), "One event should have be in list");
+    }
+
+    /**
+     * Verifies that removing from the longest queue works correctly.
+     */
+    @Test
+    void removeQueueTest() {
+        EventList eventList = new EventList() {
+            @Override
+            public void add(Event e) { }
+        };
+
+        ServicePoint sp = new ServicePoint(generator, eventList, EventType.CHECK_IN, 4);
+
+        Passenger p1 = new Passenger();
+        Passenger p2 = new Passenger();
+        Passenger p3 = new Passenger();
+        Passenger p4 = new Passenger();
+
+        sp.addQueue(p1);
+        sp.addQueue(p2);
+        sp.addQueue(p3);
+        sp.addQueue(p4);
+
+        Passenger removed = sp.removeQueue();
+
+        // One passenger should have been removed from the longest queue
+        assertNotNull(removed);
+
+        // All queues should have one passenger, except queue 0
+        assertTrue(sp.isQueueEmpty(0), "Queue 0 is not empty");
+        assertFalse(sp.isQueueEmpty(1), "Queue 1 is empty");
+        assertFalse(sp.isQueueEmpty(2), "Queue 2 is empty");
+        assertFalse(sp.isQueueEmpty(3), "Queue 3 is empty");
+    }
+
+    /**
+     * Verifies that statistics (max and average queue length) are updated.
+     */
+    @Test
+    void statisticsUpdateTest() {
+        EventList eventList = new EventList() {
+            @Override
+            public void add(Event e) { }
+        };
+
+        ServicePoint sp = new ServicePoint(generator, eventList, EventType.CHECK_IN, 2);
+
+        sp.addQueue(new Passenger());
+        sp.addQueue(new Passenger());
+        sp.addQueue(new Passenger());
+
+        // Max length should be 2 or more
+        assertTrue(sp.getMaxLength() >= 2, "Max queue length should be at least 2");
+
+        // Average length should be more than 0
+        assertTrue(sp.getAverageLength() > 0, "Average length should be positive");
     }
 }
